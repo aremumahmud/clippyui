d7fa1cffdc28291f9c9eeddb1e3fe5f78170bbc3
mahmud aremu <aremumahmud2003@gmail.com>
2024-05-13 08:42:13 +0100

    replaced server url

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,6 +1,6 @@
 const uri = {
-    // server: 'https://clippyasst.onrender.com/',
-    server: 'http://localhost:3000/',
+    server: 'https://clippyasst.onrender.com/',
+    // server: 'http://localhost:3000/',
     github: 'aremumahmud',
     instagram: 'Devsdytok',
     twitter: 'Devsdytok',
